c1cc1d945d42a17d143736091c376eaf62fbca44
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2023-02-23 11:44:51 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -954,7 +954,7 @@ async def get_pic_pil(url):
 async def build_sign_card(group:int, uid:int):
     list_my = await helper_collect.my(group, uid)
     try:
-        name = await random.choice(await get_all_have(group, uid))
+        name = random.choice(await get_all_have(group, uid))
         list_select = await get_helper_all_pic(name)
         index_ = list_my[1] - 1
         try:
